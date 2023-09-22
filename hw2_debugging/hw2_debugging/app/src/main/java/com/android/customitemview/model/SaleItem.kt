fedec90645042aea97b13905623804e033ac6c64
monsterKyu <ilkyu092@naver.com>
2023-09-22 09:38:31 +0900

    update

---
+++ hw2_debugging/hw2_debugging/app/src/main/java/com/android/customitemview/model/SaleItem.kt
@@ -0,0 +1,17 @@
+package com.android.customitemview.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SaleItem  (
+    val Image: Int,
+    val ItemTitle: String,
+    val ItemDetail: String,
+    val SellerName: String,
+    val Price: Int,
+    val Address: String,
+    var InterestCnt: Int,
+    val ChatCnt: Int,
+    var isLike: Boolean
+)  : Parcelable
